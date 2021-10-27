ce5c2f776d0a32b2cce7d1c5330736205fab3f59
ALMA_DEV914 <aungonalna58@gamil.com>
2021-10-27 10:44:27 -0700

    Add functionality to the input and button element

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ var searcButtonEl = document.querySelector("#searchbutton");
 var posterEl = document.querySelector("#poster");
 var userReviewEl = document.querySelector("#user-reviews");
 var criticsReviewEl = document.querySelector("#critics-reviews");
-var movies = inputEl.value;
 
 fetch(`https://api.themoviedb.org/3/movie/550?movies=` + inputEl.value + `&api_key=74dead5790eecd4db2f834fbf9d66bf7`)
 .then(function(response){
@@ -11,23 +10,30 @@ fetch(`https://api.themoviedb.org/3/movie/550?movies=` + inputEl.value + `&api_k
    .then(function(data){
 
       console.log(data);
-      var movies = inputEl.value;
+      var movies = inputEl.value.trim();
       
       var posterImgEl = document.createElement("img");
       var h2El = document.createElement("h2");
-      posterImgEl.setAttribute("src","https://image.tmdb.org/t/p/original/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg");
-      h2El.textContent = data.title;
+      // posterImgEl.setAttribute("src","https://image.tmdb.org/t/p/original/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg");
+      h2El.textContent = "";
+      posterImgEl.textContent = "";
       userReviewEl.textContent = data.overview;
-      criticsReviewEl.textContent = " Status: " + data.status  + "Genres: " + data.genres[0].name +  " Popularity: " + data.popularity;
+     criticsReviewEl.textContent = " Status: " + data.status  + "Genres: " + data.genres[0].name +  " Popularity: " + data.popularity;
 
-      posterEl.append(h2El);
-      posterEl.append(posterImgEl);
+      //posterEl.append(h2El);
+      //posterEl.append(posterImgEl);
    });
+});
 
-   });
-
-searcButtonEl.addEventListener = function(event){
+searcButtonEl.addEventListener("click", function(event){
    event.preventDefault();
-   var movies = inputEl.value;
    
-};
+   var movies = inputEl.value.trim();
+   console.log(movies);
+   var h2El = document.createElement("h2");
+   var posterImgEl = document.createElement("img");
+   posterImgEl.setAttribute("src","https://image.tmdb.org/t/p/original/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg");
+   h2El.textContent = movies;
+   posterEl.append(h2El);
+   posterEl.append(posterImgEl);
+});
