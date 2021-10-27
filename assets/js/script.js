609392f06f5753d59433d13a16887f81dec0ae1b
ALMA_DEV914 <aungonalna58@gamil.com>
2021-10-27 00:10:58 -0700

    Enhance poster design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,33 @@
-var inputEl = document.querySelector("#movies");
+var inputEl = document.querySelector("#movie-title");
 var searcButtonEl = document.querySelector("#searchbutton");
 var posterEl = document.querySelector("#poster");
 var userReviewEl = document.querySelector("#user-reviews");
 var criticsReviewEl = document.querySelector("#critics-reviews");
-fetch(`https://api.themoviedb.org/3/movie/550?&poster_path&title&api_key=74dead5790eecd4db2f834fbf9d66bf7`)
+var movies = inputEl.value;
+
+fetch(`https://api.themoviedb.org/3/movie/550?movies=` + inputEl.value + `&api_key=74dead5790eecd4db2f834fbf9d66bf7`)
 .then(function(response){
    response.json()
    .then(function(data){
+
       console.log(data);
-      var title = inputEl;
+      var movies = inputEl.value;
+      
       var posterImgEl = document.createElement("img");
       var h2El = document.createElement("h2");
-      
       posterImgEl.setAttribute("src","https://image.tmdb.org/t/p/original/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg");
       h2El.textContent = data.title;
       userReviewEl.textContent = data.overview;
       criticsReviewEl.textContent = " Status: " + data.status  + "Genres: " + data.genres[0].name +  " Popularity: " + data.popularity;
-      posterEl.append(posterImgEl);
+
       posterEl.append(h2El);
-    
+      posterEl.append(posterImgEl);
    });
-});
 
+   });
 
-
-searcButtonEl.addEventListener("click", function(event){
-   var title = inputEl;
-     console.log(title);
-});
+searcButtonEl.addEventListener = function(event){
+   event.preventDefault();
+   var movies = inputEl.value;
+   
+};
