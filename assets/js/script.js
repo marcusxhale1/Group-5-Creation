d856b6b8de4f17cfa814b7a6ddbb15a016a141cf
Andrewy2416 <andrewy2416@gmail.com>
2021-10-27 21:05:45 -0700

    fixed JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,29 @@ var searcButtonEl = document.querySelector("#searchbutton");
 var posterEl = document.querySelector("#poster");
 var userReviewEl = document.querySelector("#user-reviews");
 var criticsReviewEl = document.querySelector("#critics-reviews");
+//var apiTestEl = 'https://api.themoviedb.org/3/search/movie?api_key=74dead5790eecd4db2f834fbf9d66bf7&query=' + inputEl.value;
 
-fetch(`https://api.themoviedb.org/3/movie/550?movies=` + inputEl.value + `&poster_path&title&imdb_id&api_key=74dead5790eecd4db2f834fbf9d66bf7`)
-.then(function(response){
-   response.json()
-   .then(function(data){
 
-      console.log(data);
-      var movies = inputEl.value.trim();
-      var posterImgEl = document.createElement("img");
-      var h2El = document.createElement("h2");
-      var reviewsEl = document.createElement("img");
-      // posterImgEl.setAttribute("src","https://image.tmdb.org/t/p/original/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg");
-      h2El.textContent = "";
-      posterImgEl.textContent = "";
-      reviewsEl.textContent = "";
-      userReviewEl.textContent = "";
-      criticsReviewEl.textContent = "";
+//fetch('https://api.themoviedb.org/3/search/movie?api_key=74dead5790eecd4db2f834fbf9d66bf7&query=' + inputEl.value)
+
+
+
+//fetch(`https://api.themoviedb.org/3/movie/551?movies=` + inputEl.value + `&poster_path&title&imdb_id&api_key=74dead5790eecd4db2f834fbf9d66bf7`)
+// .then(function(response){
+//    response.json()
+//    .then(function(data){
+
+//       console.log(data);
+//       var movies = inputEl.value.trim();
+//       var posterImgEl = document.createElement("img");
+//       var h2El = document.createElement("h2");
+//       var reviewsEl = document.createElement("img");
+//       // posterImgEl.setAttribute("src","https://image.tmdb.org/t/p/original/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg");
+//       h2El.textContent = "";
+//       posterImgEl.textContent = "";
+//       reviewsEl.textContent = "";
+//       userReviewEl.textContent = "";
+//       criticsReviewEl.textContent = "";
 
       //posterEl.append(h2El);
       //posterEl.append(posterImgEl);
@@ -28,24 +34,57 @@ fetch(`https://api.themoviedb.org/3/movie/550?movies=` + inputEl.value + `&poste
     
    searcButtonEl.addEventListener("click", function(event){
    event.preventDefault();
+   fetch('https://api.themoviedb.org/3/search/movie?api_key=74dead5790eecd4db2f834fbf9d66bf7&query=' + inputEl.value)
+
+   .then(function(response){
+      response.json()
+      .then(function(data){
    
+         console.log(data);
+         var movies = inputEl.value.trim();
+         var posterImgEl = document.createElement("img");
+         var h2El = document.createElement("h2");
+         var reviewsEl = document.createElement("img");
+         // posterImgEl.setAttribute("src","https://image.tmdb.org/t/p/original/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg");
+         h2El.textContent = "";
+         posterImgEl.textContent = "";
+         reviewsEl.textContent = "";
+         userReviewEl.textContent = "";
+         criticsReviewEl.textContent = "";
+   
+         //posterEl.append(h2El);
+         //posterEl.append(posterImgEl);
+
+
+   
+   var idEl = data.results[0].id
    var movies = inputEl.value.trim();
    console.log(movies);
    var h2El = document.createElement("h2");
    var posterImgEl = document.createElement("img");
-   var reviewsEl = document.createElement("img");
+   //var reviewsEl = document.createElement("img");
    var overviewEl = document.createElement("p");
-   posterImgEl.setAttribute("src", "https://image.tmdb.org/t/p/original/" + data.poster_path);
-   reviewsEl.setAttribute("src", "https://image.tmdb.org/t/p/original/" + data.backdrop_path);
+   posterImgEl.setAttribute("src", "https://image.tmdb.org/t/p/original/" + data.results[0].poster_path);
+   //reviewsEl.setAttribute("src", "https://image.tmdb.org/t/p/original/" + data.backdrop_path);
    h2El.textContent = movies;
-   overviewEl.textContent = data.overview;
-   userReviewEl.textContent = data.homepage + " " + "Tagline: " + data.tagline;
-   criticsReviewEl.textContent = "Status: "  + data.status  + " Genres: " + data.genres[0].name +  " Popularity: " + data.popularity;
+   overviewEl.textContent = data.results[0].poster_path //data.overview; 
+  // userReviewEl.textContent = data.homepage + " " + "Tagline: " + data.tagline;
+   //criticsReviewEl.textContent = "Status: "  + data.results.id  + " Genres: " + data.genres[0].name +  " Popularity: " + data.popularity;
    posterEl.append(h2El);
    posterEl.append(posterImgEl);
    extraImgEl.append(overviewEl);
-   extraImgEl.append(reviewsEl);
+   //extraImgEl.append(reviewsEl);
    
 });
    });
 });
+
+
+
+
+
+
+
+
+
+
