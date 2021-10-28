90512e2e191b3a632bc7aeb2a4d60360d427c48b
ALMA_DEV914 <aungonalna58@gamil.com>
2021-10-27 17:09:58 -0700

    Add backdrop image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,51 @@
+var extraImgEl = document.querySelector("#extra-img");
 var inputEl = document.querySelector("#movie-title");
 var searcButtonEl = document.querySelector("#searchbutton");
 var posterEl = document.querySelector("#poster");
 var userReviewEl = document.querySelector("#user-reviews");
 var criticsReviewEl = document.querySelector("#critics-reviews");
 
-fetch(`https://api.themoviedb.org/3/movie/550?movies=` + inputEl.value + `&api_key=74dead5790eecd4db2f834fbf9d66bf7`)
+fetch(`https://api.themoviedb.org/3/movie/550?movies=` + inputEl.value + `&poster_path&title&imdb_id&api_key=74dead5790eecd4db2f834fbf9d66bf7`)
 .then(function(response){
    response.json()
    .then(function(data){
 
       console.log(data);
       var movies = inputEl.value.trim();
-      
       var posterImgEl = document.createElement("img");
       var h2El = document.createElement("h2");
+      var reviewsEl = document.createElement("img");
       // posterImgEl.setAttribute("src","https://image.tmdb.org/t/p/original/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg");
       h2El.textContent = "";
       posterImgEl.textContent = "";
-      userReviewEl.textContent = data.overview;
-     criticsReviewEl.textContent = " Status: " + data.status  + "Genres: " + data.genres[0].name +  " Popularity: " + data.popularity;
+      reviewsEl.textContent = "";
+      userReviewEl.textContent = "";
+      criticsReviewEl.textContent = "";
 
       //posterEl.append(h2El);
       //posterEl.append(posterImgEl);
-   });
-});
-
-searcButtonEl.addEventListener("click", function(event){
+   
+    
+   searcButtonEl.addEventListener("click", function(event){
    event.preventDefault();
    
    var movies = inputEl.value.trim();
    console.log(movies);
    var h2El = document.createElement("h2");
    var posterImgEl = document.createElement("img");
-   posterImgEl.setAttribute("src","https://image.tmdb.org/t/p/original/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg");
+   var reviewsEl = document.createElement("img");
+   var overviewEl = document.createElement("p");
+   posterImgEl.setAttribute("src", "https://image.tmdb.org/t/p/original/" + data.poster_path);
+   reviewsEl.setAttribute("src", "https://image.tmdb.org/t/p/original/" + data.backdrop_path);
    h2El.textContent = movies;
+   overviewEl.textContent = data.overview;
+   userReviewEl.textContent = data.homepage + " " + "Tagline: " + data.tagline;
+   criticsReviewEl.textContent = "Status: "  + data.status  + " Genres: " + data.genres[0].name +  " Popularity: " + data.popularity;
    posterEl.append(h2El);
    posterEl.append(posterImgEl);
+   extraImgEl.append(overviewEl);
+   extraImgEl.append(reviewsEl);
+   
+});
+   });
 });
