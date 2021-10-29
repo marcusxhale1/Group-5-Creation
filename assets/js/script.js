5a449e0eb68f22b5876c4b58a17239ef78d56dec
ALMA_DEV914 <aungonalna58@gamil.com>
2021-10-29 10:33:23 -0700

    correct some link text

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,29 +6,6 @@ var userReviewEl = document.querySelector("#plot");
 var criticsReviewEl = document.querySelector("#critic-ratings");
 
 var searchMovies = [];
-//fetch('https://api.themoviedb.org/3/search/movie?api_key=74dead5790eecd4db2f834fbf9d66bf7&query=' + inputEl.value)
-
-
-
-//fetch(`https://api.themoviedb.org/3/movie/551?movies=` + inputEl.value + `&poster_path&title&imdb_id&api_key=74dead5790eecd4db2f834fbf9d66bf7`)
-// .then(function(response){
-//    response.json()
-//    .then(function(data){
-
-//       console.log(data);
-//       var movies = inputEl.value.trim();
-//       var posterImgEl = document.createElement("img");
-//       var h2El = document.createElement("h2");
-//       var reviewsEl = document.createElement("img");
-//       // posterImgEl.setAttribute("src","https://image.tmdb.org/t/p/original/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg");
-//       h2El.textContent = "";
-//       posterImgEl.textContent = "";
-//       reviewsEl.textContent = "";
-//       userReviewEl.textContent = "";
-//       criticsReviewEl.textContent = "";
-
-      //posterEl.append(h2El);
-      //posterEl.append(posterImgEl);
    
 searchButtonEl.addEventListener("click", function(event){
    event.preventDefault();
@@ -43,15 +20,7 @@ searchButtonEl.addEventListener("click", function(event){
          var posterImgEl = document.createElement("img");
          var h2El = document.createElement("h2");
          var reviewsEl = document.createElement("img");
-         // posterImgEl.setAttribute("src","https://image.tmdb.org/t/p/original/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg");
-         //h2El.textContent = "";
-         //posterImgEl.textContent = "";
-         //reviewsEl.textContent = "";
-         //userReviewEl.textContent = "";
-         //criticsReviewEl.textContent = "";
-         //posterEl.append(h2El);
-         //posterEl.append(posterImgEl);
-   //var ratings = imdbID;
+         
    var movies = inputEl.value.trim();
    console.log(movies);
    var h2El = document.createElement("h2");
@@ -62,11 +31,10 @@ searchButtonEl.addEventListener("click", function(event){
    reviewsEl.setAttribute("src", "https://image.tmdb.org/t/p/original/" + data.results[0].backdrop_path);
    h2El.textContent = movies;
    userReviewEl.textContent = data.results[0].overview;
-  // criticsReviewEl.textContent = "Released date: "  + data.results[0].release_date +  " Popularity: " + data.results[0].popularity + " Votes Average: " + data.results[0].vote_average + " Vote Count: " + data.results[0].vote_count;
+  
    posterEl.append(h2El);
    posterEl.append(posterImgEl);
-   criticsReviewEl.appendChild(ratingEl)
-   //extraImgEl.append(reviewsEl);
+   criticsReviewEl.appendChild(ratingEl);
    
    });
 });
@@ -78,7 +46,7 @@ fetch("http://www.omdbapi.com/?t=" + inputEl.value + "&apikey=f46c4fdd")
       
       
       var ratingEl = document.createElement("p")
-       criticsReviewEl.textContent = "ImdbRating: " + data.imdbRating;
+         criticsReviewEl.textContent = "ImdbRating: " + data.imdbRating;
          ratingEl.textContent = "Internet Movie Database: " + data.Ratings[0].Value;
          criticsReviewEl.appendChild(ratingEl);
         
@@ -89,18 +57,18 @@ fetch("http://www.omdbapi.com/?t=" + inputEl.value + "&apikey=f46c4fdd")
 function saveMovies(){
    var movies = inputEl.value.trim();
    if(searchMovies.indexOf(movies)===-1){
-      searchMovies.push(movies)
+      searchMovies.push(movies);
       localStorage.setItem("title", JSON.stringify(searchMovies));
    }
    return searchMovies.value;
 }
-function loadCities(){
+function loadMovies(){
    posterEl.innerHTML = "";
    var loadMovies =   JSON.parse(localStorage.getItem("title")) || [];
    
    for (let i = 0; i < loadMovies.length; i++) {
        
-       var movies = loadMovies[i];
+   var movies = loadMovies[i];
 
    
    searchButtonEl.addEventListener("click", function(event){
@@ -112,7 +80,7 @@ function loadCities(){
 
    }
 }
-loadCities();
+loadMovies();
 
 }); 
 
