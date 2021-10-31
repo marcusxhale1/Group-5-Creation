5ed2798e93f803d22994374ed7bea865bc0cfa50
ALMA_DEV914 <aungonalna58@gamil.com>
2021-10-31 14:02:47 -0700

    Add section to store movie title history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
-var extraImgEl = document.querySelector("#extra-img");
+
 var inputEl = document.querySelector("#movie-title");
 var searchButtonEl = document.querySelector("#searchbutton");
 var posterEl = document.querySelector("#poster");
 var userReviewEl = document.querySelector("#plot");
-var criticsReviewEl = document.querySelector("#critic-ratings");
-
+var criticsReviewEl = document.querySelector
+("#critic-ratings");
+var dropdownEl = document.querySelector("#dropdown");
 var searchMovies = [];
-   
+
 searchButtonEl.addEventListener("click", function(event){
    event.preventDefault();
-   fetch('https://api.themoviedb.org/3/search/movie?api_key=74dead5790eecd4db2f834fbf9d66bf7&query=' + inputEl.value)
+   var movies = inputEl.value.trim(); 
+   posterEl.textContent = "";
+   userReviewEl.textContent = "";
+   criticsReviewEl.textContent = "";
+   saveMovies();
+   title(movies);
+   ratings(movies);
+
+});
+function title(movies){
+   fetch('https://api.themoviedb.org/3/search/movie?api_key=74dead5790eecd4db2f834fbf9d66bf7&query=' + movies)
 
    .then(function(response){
       response.json()
@@ -22,13 +33,12 @@ searchButtonEl.addEventListener("click", function(event){
          var reviewsEl = document.createElement("img");
          
    var movies = inputEl.value.trim();
-   console.log(movies);
+   //console.log(movies);
    var h2El = document.createElement("h2");
    var posterImgEl = document.createElement("img");
-   var reviewsEl = document.createElement("img");
    var ratingEl = document.createElement("p")
    posterImgEl.setAttribute("src", "https://image.tmdb.org/t/p/original/" + data.results[0].poster_path);
-   reviewsEl.setAttribute("src", "https://image.tmdb.org/t/p/original/" + data.results[0].backdrop_path);
+
    h2El.textContent = movies;
    userReviewEl.textContent = data.results[0].overview;
   
@@ -38,21 +48,22 @@ searchButtonEl.addEventListener("click", function(event){
    
    });
 });
-fetch("http://www.omdbapi.com/?t=" + inputEl.value + "&apikey=f46c4fdd")
+};
+function ratings(movies){
+fetch("http://www.omdbapi.com/?t=" + movies + "&apikey=f46c4fdd")
 .then(function(response){
    response.json()
    .then(function(data){
         console.log(data);
-      
       
       var ratingEl = document.createElement("p")
          criticsReviewEl.textContent = "ImdbRating: " + data.imdbRating;
          ratingEl.textContent = "Internet Movie Database: " + data.Ratings[0].Value;
          criticsReviewEl.appendChild(ratingEl);
         
-   })
-   saveMovies();
-})
+   });
+});
+}
 
 function saveMovies(){
    var movies = inputEl.value.trim();
@@ -67,22 +78,39 @@ function loadMovies(){
    var loadMovies =   JSON.parse(localStorage.getItem("title")) || [];
    
    for (let i = 0; i < loadMovies.length; i++) {
-       
-   var movies = loadMovies[i];
-
-   
-   searchButtonEl.addEventListener("click", function(event){
+       var movietitle = loadMovies[i];
+    
+    var moviesButtonEl = document.createElement("li");
+    moviesButtonEl.setAttribute("value", loadMovies[i]);
+     moviesButtonEl.textContent =  movietitle;
+     moviesButtonEl.className = "list-item"
+   moviesButtonEl.addEventListener("click", function(event){
+      event.preventDefault();
        posterEl.innerHTML = "";
-       $(loadMovies);
-       event.preventDefault();
+       userReviewEl.innerHTML = "";
+       criticsReviewEl.innerHTML = "";
+       var movies = this.textContent;
+       title(movies);
+       ratings(movies);
        
-   })
-
+   });
+   dropdownEl.append(moviesButtonEl);
    }
 }
+function showHistory() {
+   var loadMovies = JSON.parse(localStorage.getItem("title")) || [];
+   for (let i = 0; i < loadMovies.length; i++) {
+     loadMovies.innerHTML = "a";
+     loadMovies.setAttribute("value", loadMovies[i]);
+     cityContainerEl.append(loadMovies[i]);
+   }
+   dropdownEl.textContent = loadMovies;
+ }
+ 
+ 
 loadMovies();
 
-}); 
+//}); 
 
 
 
